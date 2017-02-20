35c8d122e0f3e3711731740e5321a514f8f32587
Brandon Cote <bcote1996@gmail.com>
2017-02-20 15:07:28 -0500

    progress

--- sp17-EGP300-Graphics2-FW-dev/EGP Graphics FW Class Base/source/egpfw/egpfwOBJLoader.c
+++ sp17-EGP300-Graphics2-FW-dev/EGP Graphics FW Class Base/source/egpfw/egpfwOBJLoader.c
@@ -66,7 +66,157 @@ typedef struct uniqueNormal		uniqueNormal;
 egpTriOBJDescriptor egpfwLoadTriangleOBJ(const char *objPath, const egpMeshNormalMode normalMode, const double globalScale)
 {
 	egpTriOBJDescriptor obj = { 0 };
-	//...
+
+	FILE *file = fopen(objPath, "r");
+
+	if (file == NULL)
+		printf("\nCould not open file %s!\n", objPath);
+
+
+	fseek(file, 0L, SEEK_END);
+	int fileSize = ftell(file);
+	rewind(file);
+
+	float* theData = (float*)malloc(fileSize);
+
+	obj.attribOffset[ATTRIB_POSITION] = 0;
+
+	int count = 0;
+	int currAttrIndex = 0;
+	char* str = (char*)malloc(sizeof(char) * 100);
+	fgets(str, 100, file);
+	if (str == NULL)
+		printf("Cound not read from OBJ file!\n");
+
+	while (!(str[0] == 'v' && str[1] == ' '))
+	{
+		fgets(str, 100, file);
+		if (str == NULL)
+			printf("Could not read from OBJ file");
+
+		printf("\n%s", str);
+	}
+
+	if (str == NULL)
+		printf("No OBJ verteces could be found in %s!\n", objPath);
+
+
+	char* num = (char*)malloc(sizeof(char) * 10);
+
+	int currStrIndex;
+	int currNumIndex = 0;
+
+	while (str[0] == 'v' && str[1] == ' ')
+	{
+		currStrIndex = 2;
+
+		for (int i = 0; i < 3; i++)
+		{
+			while (str[currStrIndex] != ' ')
+				num[currNumIndex++] = str[currStrIndex++];
+
+			float temp = strtof(num, NULL);
+			theData[currAttrIndex] = temp;
+
+			for (int j = 0; j < 10; j++)
+				num[j] = 0;
+
+			currNumIndex = 0;
+			currStrIndex++;
+			count++;
+			currAttrIndex++;
+		}
+
+
+		fgets(str, 100, file);
+		if (str == NULL)
+			printf("Could not read from OBJ file");
+	}
+
+	count = 0;
+	obj.attribOffset[ATTRIB_TEXCOORD] = currAttrIndex;
+
+	while (str[0] == 'v' && str[1] == 't' && str[2] == ' ')
+	{
+		currStrIndex = 3;
+		for (int i = 0; i < 2; i++)
+		{
+			while (str[currStrIndex] != ' ')
+				num[currNumIndex++] = str[currStrIndex++];
+
+			float temp = strtof(num, NULL);
+			printf("%f\n", temp);
+			theData[currAttrIndex] = temp;
+
+			for (int j = 0; j < 10; j++)
+				num[j] = 0;
+
+			currNumIndex = 0;
+			currStrIndex++;
+			count++;
+			currAttrIndex++;
+		}
+
+		fgets(str, 100, file);
+		if (str == NULL)
+			printf("Could not read from OBJ file");
+
+		printf("\n%s", str);
+	}
+
+	count = 0;
+	obj.attribOffset[ATTRIB_NORMAL] = currAttrIndex;
+
+	while (str[0] == 'v' && str[1] == 'n' && str[2] == ' ')
+	{
+		currStrIndex = 3;
+
+		for (int i = 0; i < 3; i++)
+		{
+			while (str[currStrIndex] != ' ')
+				num[currNumIndex++] = str[currStrIndex++];
+
+			float temp = strtof(num, NULL);
+			theData[currAttrIndex] = temp;
+
+			for (int j = 0; j < 10; j++)
+				num[j] = 0;
+
+			currNumIndex = 0;
+			currStrIndex++;
+			count++;
+			currAttrIndex++;
+		}
+
+
+		fgets(str, 100, file);
+		if (str == NULL)
+			printf("Could not read from OBJ file");
+	}
+
+	while (!(str[0] == 'f' && str[1] == ' '))
+	{
+		fgets(str, 100, file);
+		if (str == NULL)
+			printf("Could not read from OBJ file");
+
+		printf("\n%s", str);
+	}
+
+
+
+	for (int j = 0; j < count + obj.attribOffset[ATTRIB_NORMAL]; j++)
+	{
+		printf("%f\n", theData[j]);
+	}
+
+
+
+	fclose(file);
+	free(str);
+	free(num);
+
+	obj.data = theData;
 	return obj;
 }
 
@@ -75,7 +225,19 @@ egpTriOBJDescriptor egpfwLoadTriangleOBJ(const char *objPath, const egpMeshNorma
 // convert OBJ to VAO & VBO
 int egpfwCreateVAOFromOBJ(const egpTriOBJDescriptor *obj, egpVertexArrayObjectDescriptor *vao_out, egpVertexBufferObjectDescriptor *vbo_out)
 {
-	//...
+	egpAttributeDescriptor attribs[] = {
+		(ATTRIB_POSITION, ATTRIB_VEC3, egpfwGetOBJAttributeData(obj, ATTRIB_POSITION)),
+		(ATTRIB_COLOR, ATTRIB_VEC3, egpfwGetOBJAttributeData(obj, ATTRIB_COLOR)),
+		(ATTRIB_NORMAL, ATTRIB_VEC3, egpfwGetOBJAttributeData(obj, ATTRIB_NORMAL)),
+		(ATTRIB_TEXCOORD, ATTRIB_VEC2, egpfwGetOBJAttributeData(obj, ATTRIB_TEXCOORD)),
+	};
+
+	//junk ? creating a vao requires
+	egpIndexBufferObjectDescriptor* ibo;
+
+	 *vao_out = egpfwCreateVAOInterleaved(PRIM_TRIANGLES, attribs, 4, egpfwGetOBJNumVertices(obj), vbo_out , ibo);
+
+	egpfwReleaseOBJ(obj);
 	return 0;
 }
 
@@ -84,7 +246,10 @@ int egpfwCreateVAOFromOBJ(const egpTriOBJDescriptor *obj, egpVertexArrayObjectDe
 // free obj data
 int egpfwReleaseOBJ(egpTriOBJDescriptor *obj)
 {
-	//...
+	//... Here
+
+	free(obj);
+
 	return 0;
 }
 
@@ -93,14 +258,13 @@ int egpfwReleaseOBJ(egpTriOBJDescriptor *obj)
 // save/load binary
 int egpfwSaveBinaryOBJ(const egpTriOBJDescriptor *obj, const char *binPath)
 {
-	//...
+	
 	return 0;
 }
 
 egpTriOBJDescriptor egpfwLoadBinaryOBJ(const char *binPath)
 {
 	egpTriOBJDescriptor obj = { 0 };
-	//...
 	return obj;
 }
 
